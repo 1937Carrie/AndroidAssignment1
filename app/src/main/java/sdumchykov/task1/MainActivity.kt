1bda0c4f0c498ae8a9100df2a8e1d233e652d622
Stanislav Dumchykov <mechanikea@gmail.com>
2022-12-10 14:56:40 +0200

    Added some elements to layout

--- app/src/main/java/sdumchykov/task1/MainActivity.kt
+++ app/src/main/java/sdumchykov/task1/MainActivity.kt
@@ -1,21 +1,14 @@
 package sdumchykov.task1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val layout: ConstraintLayout = findViewById(R.id.constraintLayout)
-        val textView = TextView(this)
-        textView.setText("Hello I am Text View")
-        textView.textSize = 30F
-//        layout.addView(textView)
 
     }
 }
