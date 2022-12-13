5998101d34d83f6d490993dec09acd749e6ecc4a
Stanislav Dumchykov <mechanikea@gmail.com>
2022-12-13 17:24:00 +0200

    Worked out different states of the EditProfile button

--- app/src/main/java/sdumchykov/task1/MainActivity.kt
+++ app/src/main/java/sdumchykov/task1/MainActivity.kt
@@ -1,13 +1,18 @@
 package sdumchykov.task1
 
 import android.annotation.SuppressLint
-import android.graphics.drawable.Drawable
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
+    private var stockBackground = true
+    private var maestroCounter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,8 +22,42 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("ResourceAsColor")
     fun onClickEditProfile(view: View) {
         val bottomView: View = findViewById(R.id.view_bottom_container)
-        val background: Drawable = bottomView.background
-        bottomView.setBackgroundColor(0xFFAAAA)
+        val profileImage: ImageView = findViewById(R.id.image_picture)
+
+        val listOfColors = listOf(
+            Color.YELLOW,
+            Color.BLACK,
+            Color.BLUE,
+            Color.CYAN,
+            Color.DKGRAY,
+            Color.MAGENTA,
+            Color.RED,
+            Color.GREEN,
+            Color.GRAY
+        )
+        val listOfMaestro = listOf(
+            R.drawable.maestro0,
+            R.drawable.maestro1,
+            R.drawable.maestro2,
+            R.drawable.maestro3,
+            R.drawable.maestro4
+        )
+
+        val nextInt = Random.nextInt(listOfColors.size)
+        val randomColor = listOfColors[nextInt]
+
+        if (stockBackground) {
+            stockBackground = false
+            bottomView.setBackgroundColor(randomColor)
+
+            val currentImage = listOfMaestro[maestroCounter++ % 5]
+            profileImage.setImageResource(currentImage)
+
+        } else {
+            stockBackground = true
+            profileImage.setImageResource(R.drawable.tmp_rounded_image)
+            bottomView.setBackgroundColor(0xFFFAFAFA.toInt())
+        }
 
     }
 
