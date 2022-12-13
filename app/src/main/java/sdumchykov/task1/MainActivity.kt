e5fe1b27f4242838e3982eb6d6f25f0d2a2b6214
Stanislav Dumchykov <mechanikea@gmail.com>
2022-12-13 16:16:03 +0200

    Tried to move elements using activity

--- app/src/main/java/sdumchykov/task1/MainActivity.kt
+++ app/src/main/java/sdumchykov/task1/MainActivity.kt
@@ -1,6 +1,9 @@
 package sdumchykov.task1
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -8,6 +11,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    @SuppressLint("ResourceAsColor")
+    fun onClickEditProfile(view: View) {
+        val bottomView: View = findViewById(R.id.view_bottom_container)
+        val background: Drawable = bottomView.background
+        bottomView.setBackgroundColor(0xFFAAAA)
 
     }
 
