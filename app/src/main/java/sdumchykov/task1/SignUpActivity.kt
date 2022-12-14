16d7857ec9f677f21b29eb26b80c56f0fbd914e6
Stanislav Dumchykov <mechanikea@gmail.com>
2022-12-14 11:50:04 +0200

    Commit dated 12:00 14.12.2022

--- app/src/main/java/sdumchykov/task1/SignUpActivity.kt
+++ app/src/main/java/sdumchykov/task1/SignUpActivity.kt
@@ -1,11 +1,31 @@
 package sdumchykov.task1
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.SignInButton
 
-class SignUpActivity:AppCompatActivity() {
+
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val signInButton: SignInButton = findViewById(R.id.sign_in_button)
+
+        setGooglePlusButtonText(signInButton, "GOOGLE")
+
+    }
+
+    private fun setGooglePlusButtonText(signInButton: SignInButton, buttonText: String) {
+        for (i in 0 until signInButton.childCount) {
+            val v: View = signInButton.getChildAt(i)
+
+            if (v is TextView) {
+                v.text = buttonText
+                return
+            }
+        }
     }
 }
